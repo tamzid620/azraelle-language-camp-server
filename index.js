edca4a4a6665eaf09ed3b1a0964e1e26a54f21b5
tamzid620 <tamzid620@gmail.com>
2023-06-08 12:15:02 +0600

    courses data send to frontend to get popular classes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -7,7 +9,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb section ------------------------------
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtemx5j.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const courseCollection = client.db("AzraelleCamp").collection("courses");
+    app.get('/courses' , async(req, res) => {
+        const result = await courseCollection.find().toArray();
+        res.send(result);
+    }) 
+
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // formal get and listen section --------------------------
