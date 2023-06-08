b0ba77d776da53e2ea67c6deb3b7fba7ad172448
tamzid620 <tamzid620@gmail.com>
2023-06-09 01:04:00 +0600

    popular classes get req  send to the frontend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient , ServerApiVersion} = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb section ------------------------------
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtemx5j.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,23 +25,53 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server (optional starting in v4.7)
+    const classCollection = client.db('Azrealle').collection('classes');
 
-    const courseCollection = client.db("AzraelleCamp").collection("courses");
-    app.get('/courses' , async(req, res) => {
-        const result = await courseCollection.find().toArray();
+    // get all info from api -------------------
+    app.get('/classes', async (req, res) => {
+      try {
+        const result = await classCollection.find().sort({ total_students: -1 }).toArray();
         res.send(result);
-    }) 
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+//popular classes section -------------------
+   app.get('/popularclasses', async (req, res) => {
+      try {
+        const result = await classCollection.find().limit(6).sort({ total_students: -1 }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+//popular instructor section -------------------
+app.get('/popularinstructor', async (req, res) => {
+  try {
+    const result = await classCollection.find().limit(6).sort({ total_students: -1 }).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
     await client.connect();
-    
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-
-    // await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
-run().catch(console.dir);
+
+run().catch(console.error);
+
 
 
 // formal get and listen section --------------------------
